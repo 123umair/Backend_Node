51a4de0abdc5aa5386650a8f46af03f2d6b6cbaa
@123umair <khanuumair91@gmail.com>
2026-01-29 22:47:58 +0500

    Creating a schema and collections

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -10,3 +10,20 @@ async function main() {
     //.connect() method ia a async process so therefore it takes times for execution like database etc or any other actions...
     //it returns a promise so therefore we can handle by .then .catch these method
 }
+
+
+// now we are working on the Schema a blueprint for a collection..
+// here we create a useschema like a blue print where we stored the data in these collection document..
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    age:Number,
+});
+
+// ok now we create a Collection here.
+// Generally we kept the same name of User of model with same to the colleciton name "User" 
+// models is a class that is present inside the mongoose where it can return a newsubclass name User that we will create a documents
+const User = mongoose.model("User",userSchema)
+// after we can executed these code a collection is created in a mongodb with minimal changes the Collection name "User" can change a plural and to normal text "users" 
+const Employee = mongoose.model("Employee",userSchema) //employees colleciton name created 
