5b12073fd55ff8864052fce9f0779e9aa4bbc04a
@123umair <khanuumair91@gmail.com>
2026-02-02 22:21:59 +0500

    updated

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -6,7 +6,7 @@ main()
     }).catch((err)=> console.log(err));
 
 async function main() {
-    mongoose.connect("mongodb://127.0.0.1:27017/UmairMongo")  // now we connect with mongodb database named UmairMOngo
+   await  mongoose.connect("mongodb://127.0.0.1:27017/UmairMongo")  // now we connect with mongodb database named UmairMOngo
     //.connect() method is a async process so therefore it takes times for execution like database etc or any other actions...
     //it returns a promise so therefore we can handle by .then .catch these method
 }
